5f1752b390cedb232eb0365c59b854b944c7d8cc
mgalleconestoga <mgalle@conestogac.on.ca>
2021-05-26 10:27:04 -0700

    added login.html, login.php, request_access.php

---
+++ php/login.php
@@ -0,0 +1,17 @@
+<?php
+$submitted = !empty($_POST);
+?>
+<!DOCTYPE html> 
+<html>
+    <head>
+        <title>Login handler page</title>
+    </head>
+    <body>
+        <p>Form submitted? <?php echo (int) $submitted; ?></p>
+        <p>Your login info is</p>
+        <ul>
+            <li><b>username: </b> <?php echo $_POST['username']; ?></li>
+            <li><b>password: </b> <?php echo $_POST['password']; ?></li>
+        </ul>
+    </body>
+</html>
