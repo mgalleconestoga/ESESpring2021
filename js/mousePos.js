1a23f67e5971e6d1b113c9d7ef49e6b11119c65b
mgalleconestoga <mgalle@conestogac.on.ca>
2021-06-10 09:17:07 -0700

    JavaScript lecture 2 examples added (2)

--- js/mousePos.js
+++ js/mousePos.js
@@ -4,11 +4,11 @@ msg += '<p>height: ' + window.innerHeight + '</p>';
 msg += '<h2>History</h2><p>Pages in history: ' + window.history.length + '</p>';
 msg += '<h2>Screen</h2><p>screen width: ' + window.screen.width + '</p>'; 
 msg += '<p>screen height: ' + window.screen.height + '</p>'; 
-msg += '<h2>Mouse Pointer</h2><p>x-coord: ' + window.screenX + '</p>'; 
+msg += '<h2>Broswer window</h2><p>x-coord: ' + window.screenX + '</p>'; 
 msg += '<p>y-coord: ' + window.screenY + '</p>';
 
 var el = document.getElementById('info'); 
 el.innerHTML = msg; 
 
-alert('Current page url: ' + window.location);
-window.print();
+//alert('Current page url: ' + window.location);
+//window.print();
