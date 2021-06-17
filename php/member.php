ab59465ad7256dfe50e4f150177b03ad9f992697
mgalleconestoga <mgalle@conestogac.on.ca>
2021-06-17 10:08:33 -0700

    added member.php

--- php/member.php
+++ php/member.php
@@ -4,11 +4,8 @@
     if(isset($_SESSION['username']) && isset($_SESSION['password'])) {
         // User is authenticated
         echo 'Welcome ' . $_SESSION['username'];
-
         echo '<p>Members ONLY content </p>';
         echo '<p>Please click <a href=\'logout.php\'>here</a> to log out</p>';
-
-
 
     } else {
         // User not authenticated
